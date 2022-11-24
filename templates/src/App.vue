5a22a59c68980671fdc03a5825eff4740ec025fa
iamwebdev9981 <iamwebdev9981>
2022-11-24 12:26:11 +0530

    GreaterWMS Master

---
+++ templates/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="q-app">
+    <router-view />
+  </div>
+</template>
+<script >
+import Vconsole from "vconsole";
+if (process.env.NODE_ENV !== 'production') {
+  const vConsole = new Vconsole()
+}
+
+export default {
+  name: 'App'
+}
+</script>
